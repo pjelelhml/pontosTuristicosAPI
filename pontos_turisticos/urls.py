5b8e8ed0cc1f55069d9375ec953fc3101973b2cd
phml <paulo3385@hotmail.com>
2021-10-13 17:06:16 -0400

    endpoint de atracoes criado

--- pontos_turisticos/urls.py
+++ pontos_turisticos/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers, serializers, viewsets
 from core.api.viewsets import PontoTuristicoViewSet
+from atracoes.api.viewsets import AtracoesViewSet
 
 router = routers.DefaultRouter()
 router.register(r'pontoturistico', PontoTuristicoViewSet)
+router.register(r'atracoes', AtracoesViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
