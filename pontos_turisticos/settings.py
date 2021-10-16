4207a50704175625a9b32c28bbfbfd404fc0ff35
phml <paulo3385@hotmail.com>
2021-10-16 08:51:06 -0400

    senting data to deploy

--- pontos_turisticos/settings.py
+++ pontos_turisticos/settings.py
@@ -28,7 +28,7 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['pontos-turisticos-phml.herokuapp.com/', 'localhost:8000']
+ALLOWED_HOSTS = ['pontos-tutisticos.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
