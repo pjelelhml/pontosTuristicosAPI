da48cb61c19bff2aa2192887112babe5b31478da
phml <paulo3385@hotmail.com>
2021-10-16 09:59:41 -0400

    Configuring the app

--- pontos_turisticos/settings.py
+++ pontos_turisticos/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 from decouple import config
 import os
-import django_heroku
+# import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,7 +31,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*.herokuapp.com', 'localhost', '127.0.0.1']
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
 
 
 # Application definition
