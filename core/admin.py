efd243d645999aefad9020c20231ccc9514f64ee
phml <paulo3385@hotmail.com>
2021-10-16 20:30:01 -0400

    added new attributes and relationships

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import PontoTuristico
+from .models import PontoTuristico, DocIdentificacao
+
 # Register your models here.
 
 admin.site.register(PontoTuristico)
+admin.site.register(DocIdentificacao)
--- enderecos/api/serializers.py
+++ enderecos/api/serializers.py
@@ -1,10 +1,10 @@
 from rest_framework.serializers import ModelSerializer
 from enderecos.models import Endereco
 
+
 class EnderecoSerializer(ModelSerializer):
     class Meta:
         model = Endereco
         fields = [
-            'id', 'linha1', 'linha2', 'cidade',
-            'estado', 'pais', 'latitude', 'longitude'
-        ]
+            'linha1', 'linha2', 'cidade', 'estado', 'pais',
+            'latitude', 'longitude']
